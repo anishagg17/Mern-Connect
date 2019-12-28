024f1a46d0db2645533f26141bccd0bff5bbaeb9
Anish Aggarwal <43617894+anishagg17@users.noreply.github.com>
2019-12-29 01:37:27 +0530

    Profile Creation added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -24,4 +24,58 @@ router.get('/me', auth, async (req, res) => {
   }
 });
 
+//creating profile
+router.post('/', auth, async (req, res) => {
+  try {
+    // Get fields
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.githubusername)
+      profileFields.githubusername = req.body.githubusername;
+    // Skills - Spilt into array
+    if (typeof req.body.skills !== 'undefined') {
+      profileFields.skills = req.body.skills.split(',');
+    }
+
+    // Social
+    profileFields.social = {};
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+    let profile = await Profile.findOne({
+      user: req.user.id
+    });
+    if (profile) {
+      profile = await Profile.findByIdAndUpdate(
+        {
+          user: req.user.id
+        },
+        {
+          $set: profileFields
+        },
+        {
+          new: true
+        }
+      );
+      return res.json(profile);
+    }
+    profile = await Profile.create(profileFields);
+    return res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({
+      error: err.errmsg
+    });
+  }
+});
+
 module.exports = router;
